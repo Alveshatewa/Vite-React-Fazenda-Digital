29d5fc2687ddf066a33d425031707c200b912753
Alves Hatewa <alvesmazy90@gmail.com>
2025-04-04 20:23:34 +0100

    changed files

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,11 @@ import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
 import App from './App.jsx'
-import {  AuthProvider } from './Contexts/AuthContext/AuthContext.jsx'
+
 
 createRoot(document.getElementById('root')).render(
 
-  <AuthProvider>
-    <App />
-  </AuthProvider>,
    <StrictMode>
-   <App />
- </StrictMode>,
+    <App />
+  </StrictMode>,
 )
